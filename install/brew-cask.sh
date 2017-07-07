818b1c714f1f51067de992ad474600cc31b1058e
Brendan Schulz <brendan.schulz@gmail.com>
2017-07-07 15:44:44 -0500

    Split brew installs into brew and cask install files. Add and edit programs installed.

---
+++ install/brew-cask.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env sh
+
+# Install Caskroom
+brew tap caskroom/cask
+brew install brew-cask
+brew tap caskroom/versions
+
+# Install packages
+apps=(
+  android-sdk            # 
+  android-studio         # 
+  atom                   # 
+  caffeine               # 
+  dash                   # 
+  diffmerge              # 
+  geektool               # 
+  github-desktop         # 
+  google-chrome          # 
+  google-drive           # 
+  gyazo                  # 
+  firefox                # 
+  imagealpha             # 
+  imageoptim             # 
+  intellij-idea          # 
+  intellij-idea-ce       # 
+  iterm2                 # 
+  java                   # Java Standard Edition Development Kit
+  macdown                # 
+  screenflow             # 
+  slack                  # 
+  sourcetree             # 
+  spectacle              # 
+  vagrant                # 
+  virtualbox             # 
+  vlc                    # 
+)
+brew cask install "${apps[@]}"
+
+# Quick Look Plugins
+plugins=(
+  betterzipql            # 
+  provisionql            # 
+  qlcolorcode            #  
+  qlmarkdown             # 
+  qlimagesize            # 
+  qlprettypatch          #  
+  qlstephen              # 
+  quicklook-csv          # 
+  quicklook-json         # 
+  quicklookapk           #
+  suspicious-package     # 
+  webpquicklook          # 
+)
+brew cask install "${plugins[@]}"
+
