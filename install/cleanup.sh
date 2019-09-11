53bbdfa4f53df87c0b53a403fb439dc1f2bec4c0
Brendan Schulz <bschulz@supportworks.com>
2019-09-11 15:49:26 -0500

    Remove extra files

--- install/cleanup.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# Remove Android Studio's SDK and JDK
-rm -rf ~/Library/Android
--- install/java.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Configure Java
-
-# Configure Android SDK
-sdkmanager --list
-sdkmanager "platforms;android-25"
-sdkmanager "platforms;android-26"
